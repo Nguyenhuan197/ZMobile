e30d55b79d644d5c180737b470b8e0d6110b9bca
Nguyen Huan <youremail@example.com>
2026-02-26 14:45:30 +0700

    Update V26.02.26.15

--- src/context/useThemeContext.jsx
+++ src/context/useThemeContext.jsx
@@ -1,11 +1,13 @@
 import { createContext, useEffect, useState } from "react";
 import DecodeJWT from "../services/tocken";
+import { useNavigate } from "react-router-dom";
 export const ThemeContext = createContext(null);
 
 
 export function LocalStorageUserContext({ children }) {
     const KEY_NAME_USER = import.meta.env.VITE_KEY_NAME_USER;
     const [USER, SETUSER] = useState(false);
+    const navigate = useNavigate();
 
     useEffect(() => {
         const load = () => {
@@ -31,12 +33,21 @@ export function LocalStorageUserContext({ children }) {
         SETUSER(false);
     };
 
+    // Mua nhanh
+    const handlePage = (name, price, quantity, activeImg) => {
+        const dataPay = { name, price, quantity, activeImg }
+        localStorage.setItem("data-pay", JSON.stringify(dataPay));
+        navigate("/pay");
+    }
+
+
     return (
         <ThemeContext.Provider
             value={{
                 USER,
                 signOutUser,
-                reloading
+                reloading,
+                handlePage
             }}
         >
             {children}
