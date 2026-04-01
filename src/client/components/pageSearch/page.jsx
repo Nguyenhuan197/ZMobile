170ec5c3a0fea11fb8961a780ac196b29daaf9f9
Nguyen Huan <youremail@example.com>
2026-04-01 22:34:44 +0700

    Update V26.04.01.22.34

--- src/client/components/pageSearch/page.jsx
+++ src/client/components/pageSearch/page.jsx
@@ -1,34 +1,43 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from "./productSearch.module.css";
 import useSWR from 'swr';
 import UiLoadingComponent from '../../../components/loadingComponent';
 import { Link } from 'react-router-dom';
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
+
+
 export default function ProductSearchComponent() {
     const apiUrl = import.meta.env.VITE_API_URL_BACKEND;
-
+    const [allProducts, setAllProducts] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
-    const { data: dataProducts, isLoading, error } = useSWR(
-        `${apiUrl}/api/product/view-product-phone?status=true`,
-        fetcher
+    const { data: dataProducts, isLoading, error, mutate } = useSWR(
+        searchTerm.trim() === ''
+            ? `${apiUrl}/api/product/view-all`
+            : `${apiUrl}/api/product/search-Product?keySearch=${searchTerm}`
+        , fetcher
     );
 
+    useEffect(() => {
+        const loading = () => {
+            mutate();
+            if (dataProducts?.data) {
+                setAllProducts(dataProducts?.data);
+            }
+        }
 
-    if (isLoading) return <UiLoadingComponent />;
+        loading();
+
+    }, [searchTerm, dataProducts]);
+
+
     if (error) return <div>Có lỗi xảy ra...</div>;
-    const allProducts = dataProducts?.data || [];
 
-    // Lọc theo từ khóa
-    const filteredProducts = allProducts.filter(product =>
-        product.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
 
     return (
         <div className={styles.container}>
-            {/* Thanh tìm kiếm */}
             <div className={styles.searchHeader}>
-                <h2 className={styles.title}>Tìm kiếm sản phẩm</h2>
+                <h2 className={styles.title}>{searchTerm.trim() === '' ? 'Tìm kiếm sản phẩm' : 'Đăng tìm kiếm sản phẩm'}</h2>
 
                 <div className={styles.searchBox}>
                     <input
@@ -41,45 +50,49 @@ export default function ProductSearchComponent() {
                 </div>
 
                 <p className={styles.resultCount}>
-                    Tìm thấy <strong>{filteredProducts.length}</strong> sản phẩm
+                    Tìm thấy <strong>{allProducts.length}</strong> sản phẩm
                 </p>
             </div>
 
-            {/* Danh sách sản phẩm */}
-            <div className={styles.productGrid}>
-                {filteredProducts.map(product => (
-                    <Link to={`/product/${product._id}`}
-                        key={product._id}
-                        className={styles.productCard}
-                    >
-                        <div className={styles.imageWrapper}>
-                            <img
-                                src={product.img?.secure_url}
-                                alt={product.name}
-                                className={styles.productImage}
-                            />
-                            <span className={styles.categoryBadge}>
-                                Điện thoại
-                            </span>
-                        </div>
-
-                        <div className={styles.productInfo}>
-                            <h3 className={styles.productName}>
-                                {product.name}
-                            </h3>
-                            <p className={styles.productPrice}>
-                                {product.price?.toLocaleString()}đ
-                            </p>
-                            <button className={styles.detailBtn}>
-                                Xem chi tiết
-                            </button>
-                        </div>
-                    </Link>
-                ))}
-            </div>
 
             {
-                filteredProducts.length === 0 && (
+                isLoading ? <UiLoadingComponent /> :
+                    <div className={styles.productGrid}>
+                        {allProducts.map(product => (
+                            <Link to={`/product/${product._id}`}
+                                key={product._id}
+                                className={styles.productCard}
+                            >
+                                <div className={styles.imageWrapper}>
+                                    <img
+                                        src={product.img?.secure_url}
+                                        alt={product.name}
+                                        className={styles.productImage}
+                                    />
+                                    <span className={styles.categoryBadge}>
+                                        Điện thoại
+                                    </span>
+                                </div>
+
+                                <div className={styles.productInfo}>
+                                    <h3 className={styles.productName}>
+                                        {product.name}
+                                    </h3>
+                                    <p className={styles.productPrice}>
+                                        {product.price?.toLocaleString()}đ
+                                    </p>
+                                    <button className={styles.detailBtn}>
+                                        Xem chi tiết
+                                    </button>
+                                </div>
+                            </Link>
+                        ))}
+                    </div>
+            }
+
+
+            {
+                allProducts.length === 0 && (
                     <div className={styles.noResult}>
                         <p>Không tìm thấy sản phẩm phù hợp với từ khóa của bạn.</p>
                     </div>
